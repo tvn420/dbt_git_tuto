b634290a3aaef5c31254bff4ecbedd2daa337f89
Tuong-Vi nguyen <tvn00100@gmail.com>
2024-11-15 13:27:01 +0100

    fix conflicts branch 3/4

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -2,9 +2,10 @@ select CONCAT(order_id, '_', product_id) AS order_item_id_x,
  order_id_x,
  product_id_x,
  seller_id_x,
- DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_x,
+ DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_y,
  price as unit_price_x,
  shipping_cost_x,
  quantity as item_quantity_x,
  (price * quantity) + shipping_cost as total_order_item_amount_x
+
 from {{ source('dataset_fil_rouge', 'order_item') }}
