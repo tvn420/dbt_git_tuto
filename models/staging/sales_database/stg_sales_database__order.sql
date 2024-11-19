020bf6f8736b5e9c74ee378ba6389608eebd5043
TV CA <tnguyen13@outlook.fr>
2024-11-19 18:01:20 +0000

    commit

--- models/staging/sales_database/stg_sales_database__order.sql
+++ models/staging/sales_database/stg_sales_database__order.sql
@@ -1,12 +1,12 @@
 select
 
-    order_id_y,
-    user_name as user_id_y,
-    order_status_y,
-    DATETIME(order_date, "Europe/Paris") AS order_created_at_x,
-    DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at_x,
-    DATETIME(pickup_date, "Europe/Paris") AS picked_up_at_x,
-    DATETIME(delivered_date, "Europe/Paris") AS delivered_at_x,
-    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery_x
+    order_id,
+    user_name as user_id,
+    order_status,
+    DATETIME(order_date, "Europe/Paris") AS order_created_at,
+    DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at,
+    DATETIME(pickup_date, "Europe/Paris") AS picked_up_at,
+    DATETIME(delivered_date, "Europe/Paris") AS delivered_at,
+    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery
 
 from {{ source('dataset_fil_rouge', 'order') }}
--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -1,11 +1,11 @@
-select CONCAT(order_id, '_', product_id) AS order_item_id_x,
- order_id_x,
- product_id_x,
- seller_id_x,
- DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_y,
- price as unit_price_x,
- shipping_cost_x,
- quantity as item_quantity_x,
- (price * quantity) + shipping_cost as total_order_item_amount_x
+select CONCAT(order_id, '_', product_id) AS order_item_id,
+ order_id,
+ product_id,
+ seller_id,
+ DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at,
+ price as unit_price,
+ shipping_cost,
+ quantity as item_quantity,
+ (price * quantity) + shipping_cost as total_order_item_amount
 
 from {{ source('dataset_fil_rouge', 'order_item') }}
